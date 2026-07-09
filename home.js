99706b277707307d17e6586859047f98bbb17723
Chuckj123 <Charleslin2030@gmail.com>
2026-07-09 23:58:07 +0000

    sadasdasd

---
+++ home.js
@@ -0,0 +1,37 @@
+function buttonclicky(){
+    document.getElementsByClassName(`sheet`)[0].style.display = `block`;
+}
+
+function postButtonClick(){
+    document.getElementsByClassName(`sheet`)[0].style.display = `block`;
+
+
+    //inputboxythingy
+    const itemName = document.getElementById('itemName').value;
+    const category = document.getElementById('category').value;
+    const location = document.getElementById('location').value;
+    const description = document.getElementById('description').value;
+    const data = {
+        itemName,
+        category,
+        location,
+        description
+    };
+
+    fetch('http://localhost:5000/add', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    });
+}
+
+// //loginpage
+const nameS = document.getElementById('name').value;
+const emailS = document.getElementById('emailS').value;
+const passwordS = document.getElementById('passwordS').value;
+const pn = document.getElementById('pnS').value;
+const schoolS = document.getElementById('schoolS').value;
+const zipS = document.getElementById('zipS').value;
+
+
+
---
+++ server.js
@@ -0,0 +1,32 @@
+
+const fs = require("fs");
+const express = require('express');
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const readData = () => {
+    const data = fs.readFileSync("./database.json", 'utf-8');
+    return JSON.parse(data);
+};
+
+app.post('/add', (req, res) => {
+    console.log(req.body)
+    const data = JSON.parse(fs.readFileSync("database.json", "utf8"));
+    newData = {
+        "category": req.body["category"],
+        "location": req.body["location"],
+        "description": req.body["description"]
+    }
+    data[req.body["itemName"]] = newData
+    fs.writeFileSync("database.json", JSON.stringify(data, null, 2));
+});
+
+
+app.get('/get', (req, res) => {
+    res.json(readData());
+});
+
+app.listen(5000, () => console.log('Database running on port 5000'));
