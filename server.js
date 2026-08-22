64e78f8591b875894ce93788734966020983dbb2
Dogbot02 <hdimartino27@gmail.com>
2026-08-22 16:23:10 -0400

    Initialize Firebase in server.js
    Added Firebase initialization and configuration.

--- server.js
+++ server.js
@@ -1,4 +1,26 @@
 
+
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDITGL_1FIONA5lZAkC_524ZQAx84Meb4M",
+    authDomain: "realfindit-9652f.firebaseapp.com",
+    projectId: "realfindit-9652f",
+    storageBucket: "realfindit-9652f.firebasestorage.app",
+    messagingSenderId: "315956524199",
+    appId: "1:315956524199:web:4c910d2adbc3d2681d1782",
+    measurementId: "G-PWX9Z4P5J7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 const fs = require("fs");
 const express = require('express');
 const cors = require("cors");
